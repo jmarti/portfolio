bf03b851a6ca8900ff8c3141a23e46a608e5e1bb
Jordi Martí <jordimarti@multiplica.com>
2022-07-22 18:44:34 -0500

    about bio text

---
+++ src/components/DynamicText/DynamicText.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode, useEffect, useRef, useState } from "react"
+
+type Props = {
+  children: ReactNode
+  height: number
+}
+
+const DynamicText = (props: Props) => {
+  const { children: childrenProp, height } = props
+
+  const [ready, setReady] = useState(false)
+  const [children, setChildren] = useState(childrenProp)
+  
+  const parent = useRef<any>()
+  const element = useRef<any>()
+
+  useEffect(() => {
+    setReady(false)
+
+    setTimeout(() => {
+      setChildren(childrenProp)
+      if (!parent.current || !element.current) {
+        return
+      }
+      let currentFontSize = 12
+      let overflow = false
+      element.current.style.fontSize = currentFontSize
+
+      while (!overflow && currentFontSize < 100) {
+        element.current.style.fontSize = `${currentFontSize}px`
+        overflow = parent.current.clientHeight < element.current.clientHeight
+        if (!overflow) {
+          currentFontSize = currentFontSize + 1
+        }
+      }
+      element.current.style.fontSize = `${currentFontSize - 1}px`
+      setReady(true)
+    })
+  }, [childrenProp, height, parent, element])
+
+  return (
+    <div
+      ref={parent}
+      style={{
+        height: `${height}px`,
+        opacity: ready ? 1 : 0
+      }}>
+      <p ref={element}>
+        {children}
+      </p>
+    </div>
+  )
+}
+
+export default DynamicText
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,16 +10,23 @@ import {
   container,
   left,
   right,
-  description
+  description,
+  lengthSelector,
+  activeItem
 } from './About.module.css'
+import DynamicText from '../../components/DynamicText'
 
 
 const DEFAULT_BIO_LENGTH = 1
+const BIO_LENGTH_TEXT_EMS = [1.5, 1.2, .9, .75]
 
 const AboutPage = (props: PageProps) => {
   const allBios: Queries.AllBiosQuery = useStaticQuery(graphql`
     query AllBios {
-      allMdx(filter: {fileAbsolutePath: {regex: "/content/about/"}}) {
+      allMdx(
+        filter: {fileAbsolutePath: {regex: "/content/about/"}}
+        sort: {fields: slug}
+      ) {
         nodes {
           id
           frontmatter {
@@ -31,6 +38,7 @@ const AboutPage = (props: PageProps) => {
               }
             }
           }
+          body
         }
       }
     }
@@ -42,6 +50,10 @@ const AboutPage = (props: PageProps) => {
       allBios.allMdx.nodes.length - 1 < DEFAULT_BIO_LENGTH ? allBios.allMdx.nodes.length - 1 : DEFAULT_BIO_LENGTH
     )
   }, [])
+
+  const handleLengthSelection = (selectedLength: number) => {
+    setActiveLength(selectedLength)
+  }
 
   return (
     <LayoutPage pageTitle="About" pageProps={props}>
@@ -57,13 +69,31 @@ const AboutPage = (props: PageProps) => {
                   />
               </div>
               <div className={right}>
-                <p className={description}>{allBios.allMdx.nodes[activeLength]?.frontmatter?.description}</p>
+                <div className={description}>
+                  <DynamicText height={360}>
+                    {allBios.allMdx.nodes[activeLength]?.frontmatter?.description}
+                  </DynamicText>
+                </div>
+                <ul className={lengthSelector}>
+                  {allBios.allMdx.nodes.map((node, i, arr) => (
+                    <li
+                      key={i}
+                      className={activeLength === i ? activeItem : undefined}
+                      data-helpText={i === 0 ? 'Less' : i === arr.length - 1 ? 'More' : undefined}
+                    >
+                      <button onClick={() => handleLengthSelection(i)}>
+                        <span className="srOnly">
+                          {node.frontmatter?.title} {activeLength === i && '(active)'}
+                        </span>
+                      </button>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </>
           )}
         </div>
       </article>
-      {allBios.allMdx.nodes[activeLength]?.frontmatter?.title}
     </LayoutPage>
   )
 }
